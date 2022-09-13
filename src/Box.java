053b7b12f70cb726cd78ae6065255573a026e654
Jyrgalbek10 <110243095+Jyrgalbek10@users.noreply.github.com>
2022-09-13 15:07:52 +0300

    col-ekek

--- src/Box.java
+++ src/Box.java
@@ -1,2 +1,22 @@
-package PACKAGE_NAME;public class Box {
+public class Box <T>{
+    T val;
+
+    public T getVal() {
+        return val;
+    }
+
+    public void setVal(T val) {
+        this.val = val;
+    }
+
+    public Box(T val) {
+        this.val = val;
+    }
+
+    @Override
+    public String toString() {
+        return "Box{" +
+                "val=" + val +
+                '}';
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,12 @@
 public class Main {
+
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        maethod(new Box<>(1));
+    }
+
+     static  <T> void maethod(T elem){
+         System.out.println(elem);
+
     }
 }
